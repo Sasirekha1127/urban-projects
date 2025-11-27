5d83c673212521af7e1932b91ff77c3a4a478a10
DuraiReachCopilot <thambidurai@reachcopilot.com>
2025-11-27 10:26:17 +0530

    update files

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,20 +2,8 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
+// COMPONENTS
 import NavbarUC from "./components/Navbar.jsx";
-import CartPage from "./Pages/CardPage.jsx";
-import Beauty from "./Pages/Beauty.jsx";
-
-import Salonforwomen from "../src/pages/Salonforwomen.jsx";
-
-import Revamp from "./Pages/Revamp.jsx";
-import Native from "./Pages/Native.jsx";
-import Home from "./Pages/Home.jsx";
-import BathroomCleaning from "./Pages/BathroomCleaning.jsx";
-import Wallmakeover from "./Pages/Wallmakeover.jsx";
-import Sofacleaning from "./Pages/Sofacleaning.jsx";
-import Salonpackages from "./Pages/Salonpackages.jsx";
-import SalonPage from "./Pages/SalonPage.jsx";
 import CarouselPage from "./components/Carouselpage.jsx";
 import Newnote from "./components/Newnote.jsx";
 import Mostbook from "./components/Mostbook.jsx";
@@ -29,13 +17,31 @@ import Wal from "./components/Wal.jsx";
 import Homerepair from "./components/Homerepair.jsx";
 import Load from "./components/Loading.jsx";
 
-export default function AppContent({ setHideNavbar, hideNavbar, hideSearch, setHideSearch }) {
+// PAGES PROPERLY IMPORTED
+import CartPage from "./Pages/CardPage.jsx";
+import Beauty from "./pages/Beauty.jsx";
+import Revamp from "./Pages/Revamp.jsx";
+import Native from "./Pages/Native.jsx";
+import Home from "./Pages/Home.jsx";
+import BathroomCleaning from "./Pages/BathroomCleaning.jsx";
+import Wallmakeover from "./Pages/Wallmakeover.jsx";
+import Sofacleaning from "./Pages/Sofacleaning.jsx";
+import Salonpackages from "./Pages/Salonpackages.jsx";
+import SalonPage from "./Pages/SalonPage.jsx";
+import Salonforwomen from "./pages/Salonforwomen.jsx";
+
+export default function AppContent({
+  setHideNavbar,
+  hideNavbar,
+  hideSearch,
+  setHideSearch,
+}) {
   const location = useLocation();
 
-  // pages where search box should hide
+  // Routes where search box should hide
   const hideSearchRoutes = ["/beauty", "/native", "/revamp"];
 
-  // pages where navbar should hide completely
+  // Routes where navbar must hide
   const alwaysHideNavbarRoutes = ["/cart"];
 
   useEffect(() => {
@@ -48,10 +54,10 @@ export default function AppContent({ setHideNavbar, hideNavbar, hideSearch, setH
 
   return (
     <>
-      {/* NAVBAR - show only when allowed */}
+      {/* NAVBAR (show only when allowed) */}
       {!shouldHideNavbar && <NavbarUC hideSearch={hideSearch} />}
 
-      {/* ROUTES */}
+      {/* APP ROUTES */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Load" element={<Load />} />
@@ -90,11 +96,12 @@ export default function AppContent({ setHideNavbar, hideNavbar, hideSearch, setH
           }
         />
 
+        {/* COMPONENT ROUTES */}
         <Route path="/carouselpage" element={<CarouselPage />} />
         <Route path="/Newnote" element={<Newnote />} />
         <Route path="/Mostbook" element={<Mostbook />} />
         <Route path="/Walls" element={<Walls />} />
-        <Route path="/salon" element={<Salon />} />
+        <Route path="/salon" element={<SalonPage />} /> {/* FIXED: only one salon route */}
         <Route path="/Wallpannel" element={<Wallpannel />} />
         <Route path="/Cleaningpest" element={<CleaningPest />} />
         <Route path="/Wallpannels" element={<Wallpannels />} />
@@ -102,11 +109,22 @@ export default function AppContent({ setHideNavbar, hideNavbar, hideSearch, setH
         <Route path="/Wal" element={<Wal />} />
         <Route path="/homerepair" element={<Homerepair />} />
 
+        {/* CLEANING SERVICES */}
         <Route path="/bathroom-cleaning" element={<BathroomCleaning />} />
-        <Route path="/wallmakeover" element={<Wallmakeover setHideNavbar={setHideNavbar} />} />
-        <Route path="/sofacleaning" element={<Sofacleaning setHideNavbar={setHideNavbar} />} />
-        <Route path="/salonpackages" element={<Salonpackages setHideNavbar={setHideNavbar} />} />
-        <Route path="/salon" element={<SalonPage />} />
+        <Route
+          path="/wallmakeover"
+          element={<Wallmakeover setHideNavbar={setHideNavbar} />}
+        />
+        <Route
+          path="/sofacleaning"
+          element={<Sofacleaning setHideNavbar={setHideNavbar} />}
+        />
+        <Route
+          path="/salonpackages"
+          element={<Salonpackages setHideNavbar={setHideNavbar} />}
+        />
+
+        {/* CART */}
         <Route path="/cart" element={<CartPage />} />
       </Routes>
     </>
