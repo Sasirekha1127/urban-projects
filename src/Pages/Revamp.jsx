80935981f00cecab9000e7cf32ccb4b648297099
Sasirekha S <sasirekhass26@gmail.com>
2025-12-20 13:08:14 +0530

    update files

--- src/Pages/Revamp.jsx
+++ src/Pages/Revamp.jsx
@@ -55,6 +55,8 @@ export default function Revamp({ setHideNavbar, setHideSearch }) {
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
       { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+      { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } }, // mobile
+
     ],
   };
 
